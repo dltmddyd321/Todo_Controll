9b4256c1ef8f6071380d2e677f142fb350f24f7f
dltmddyd321 <dltmddyd321@naver.com>
2021-08-20 17:40:33 +0900

    2021_08_20
    Todo List App
    
    1. 메모 작성 Layout에 휴지통 아이콘 생성.
    
    2. 휴지통 아이콘을 클릭하면 URL 또는 Image를 삭제 가능하도록 구현.

--- Todo_1/app/src/main/java/com/example/todo_1/CreateNoteActivity.java
+++ Todo_1/app/src/main/java/com/example/todo_1/CreateNoteActivity.java
@@ -103,6 +103,24 @@ public class CreateNoteActivity extends AppCompatActivity {
             setViewOrUpdateNote();
         }
 
+        findViewById(R.id.imgRemoveWebURL).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textWebURL.setText(null);
+                layoutWebURL.setVisibility(View.GONE);
+            }
+        });
+
+        findViewById(R.id.imgRemove).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageNote.setImageBitmap(null);
+                imageNote.setVisibility(View.GONE);
+                findViewById(R.id.imgRemove).setVisibility(View.GONE);
+                selectedFilePath = "";
+            }
+        });
+
         initMiscellaneous();
         setSubtitleIndicatorColor();
     }
@@ -116,6 +134,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         if(alreadyAvailableNote.getImagePath() != null && !alreadyAvailableNote.getImagePath().trim().isEmpty()) {
             imageNote.setImageBitmap(BitmapFactory.decodeFile(alreadyAvailableNote.getImagePath()));
             imageNote.setVisibility(View.VISIBLE);
+            findViewById(R.id.imgRemove).setVisibility(View.VISIBLE);
             selectedFilePath = alreadyAvailableNote.getImagePath();
         }
 
@@ -337,6 +356,7 @@ public class CreateNoteActivity extends AppCompatActivity {
                         Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
                         imageNote.setImageBitmap(bitmap);
                         imageNote.setVisibility(View.VISIBLE);
+                        findViewById(R.id.imgRemove).setVisibility(View.VISIBLE);
 
                         selectedFilePath = getPathFromUri(selectedImageUri);
                     } catch (Exception exception) {
